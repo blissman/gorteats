ad5883c1c410146133ebef8adfee37a8d2ac489e
Filip Stankovic <filips@gmail.com>
2016-02-04 14:21:55 -0500

    Make session routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   resources :restaurants
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
