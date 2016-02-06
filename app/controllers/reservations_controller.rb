3c22a3533518eb19efafc66a8b9315de26b7b03a
Chris Staikos <cstaikos@gmail.com>
2016-02-06 18:36:01 -0500

    Refactor availability check

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -16,15 +16,17 @@
     # Assign attributes to reservation but don't save yet
     @reservation.assign_attributes(reservation_params)
 
-    if (@reservation.party_size != 0) && (@reservation.party_size <= @restaurant.capacity - @restaurant.reservations.sum(:party_size) + Reservation.find(params[:id]).party_size )
+    if @restaurant.available?(@reservation)
       if @reservation.save
         redirect_to user_path(current_user)
       else
         render :edit
       end
     else
-      flash[:notice] = "Sorry, the restauraunt does not currently have enough space for your party of #{@reservation.party_size}.
-                        Total capacity is #{@restaurant.capacity}. Please try again later. Remaining capacity is
+      flash[:notice] = "Sorry, the restauraunt does not currently have enough
+                        space for your party of #{@reservation.party_size}.
+                        Total capacity is #{@restaurant.capacity}. Please try
+                        again later. Remaining capacity is
                         #{(@restaurant.capacity - @restaurant.reservations.sum(:party_size) + Reservation.find(params[:id]).party_size)}"
       render :edit
     end
@@ -40,7 +42,7 @@
       restaurant: @restaurant
     )
 
-    if (@reservation.party_size != 0) && @reservation.party_size <= (@restaurant.capacity - @restaurant.reservations.sum(:party_size))
+    if @restaurant.available?(@reservation)
 
       if @reservation.save
         redirect_to user_path(current_user)
@@ -49,9 +51,12 @@
       end
 
     else
-      flash[:notice] = "Sorry, the restauraunt does not currently have enough space for your party of #{@reservation.party_size}.
-                        Total capacity is #{@restaurant.capacity}. Please try again later. Remaining capacity is
+      flash[:notice] = "Sorry, the restauraunt does not currently have enough
+                        space for your party of #{@reservation.party_size}.
+                        Total capacity is #{@restaurant.capacity}. Please try
+                        again later. Remaining capacity is
                         #{(@restaurant.capacity - @restaurant.reservations.sum(:party_size))}"
+
       redirect_to @restaurant
     end
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,14 @@ class Restaurant < ActiveRecord::Base
   has_many :reservations
   has_many :users, through: :reservations
   belongs_to :owner, class_name: 'User'
+
+  def available? (reservation)
+    exists = reservation.id ? true : false
+
+    if exists
+      (reservation.party_size != 0) && (reservation.party_size <= capacity - reservations.sum(:party_size) + Reservation.find(reservation.id).party_size)
+    else
+      (reservation.party_size != 0) && (reservation.party_size <= capacity - reservations.sum(:party_size))
+    end
+  end
 end
