12a7b1f5850cf19d3b044fecb15626e12a6a975f
Filip Stankovic <filips@gmail.com>
2016-02-06 17:36:10 -0500

    Cleanup unused code in manage restaurants controller

--- app/controllers/manage/restaurants_controller.rb
+++ app/controllers/manage/restaurants_controller.rb
@@ -19,21 +19,4 @@ class Manage::RestaurantsController < ApplicationController
     @user = current_user
     @business = @user.businesses.find(params[:id])
   end
-
-  # def update
-  #   @user = current_user
-  #   @business = @user.businesses.find(params[:id])
-  #   if @business.update_attributes(business_params)
-  #     redirect_to manage_restaurants_path, notice: "Restaurant updated!"
-  #   else
-  #     render :show, notice: 'Invalid input.'
-  #   end
-  # end
-
-  # private
-  # def business_params
-  #   params.require(:restaurant).permit(:name, :location, :price, :description)
-  # end
-
-
 end
